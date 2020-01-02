8a31c5dc91db82a067e33d6659ef4b6e9a7ac238
Tobias Jennerjahn <tobias@jennerjahn.xyz>
2020-01-02 21:09:55 +0100

    changed position of curly braces

--- keymaps/de_velopment/keymap.c
+++ keymaps/de_velopment/keymap.c
@@ -88,17 +88,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   °  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   {  |   [  |   ]  |   }  |  \   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |   [  |   ]  |      |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   (  |   )  |      |Enter |
+ * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   {  |   )  |   }  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_plaid_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DE_LCBR, DE_LBRC, DE_RBRC, DE_RCBR , DE_BSLS,
-  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, DE_LPRN, DE_RPRN, _______, KC_ENT,
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, DE_LBRC, DE_RBRC, _______ , DE_BSLS,
+  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, DE_LCBR, DE_RCBR, _______, KC_ENT,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_plaid_grid(
-  DE_CIRC, DE_EXLM, DE_DQOT, DE_PARA, DE_DLR,  DE_PERC, DE_AMPR, DE_SLSH, DE_LPRN, DE_RPRN, DE_EQL, KC_BSPC,
+  DE_CIRC, DE_EXLM, DE_DQOT, DE_PARA, DE_DLR,  DE_PERC, DE_AMPR, DE_SLSH, DE_EQL, DE_LPRN, DE_RPRN, KC_BSPC,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DE_AT  , DE_EURO, KC_LBRC, DE_QST, DE_QUOT,
   KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, DE_PIPE , KC_SCLN, KC_QUOT, _______, KC_ENT,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
